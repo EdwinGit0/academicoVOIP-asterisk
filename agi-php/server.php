8a1ace4579d691bc59c9d38787aa5916fbd962a6
Edwin Roque Cerrogrande <53125315+EdwinGit0@users.noreply.github.com>
2022-10-19 22:30:38 -0400

    Update server.php

--- agi-php/server.php
+++ agi-php/server.php
@@ -2,7 +2,7 @@
    class server{
 
       protected static function post($jsonData,$option){
-         $url = "http://app-873282b0-236c-480e-add3-7d08817fc520.cleverapps.io/".$option;
+         $url = "http://app-63c20f87-add9-4bba-bb8a-47b1404f9ad6.cleverapps.io/".$option;
          $ch = curl_init($url);
          curl_setopt($ch, CURLOPT_POSTFIELDS, $jsonData);
          curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
